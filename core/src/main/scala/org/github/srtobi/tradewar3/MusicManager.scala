0b5e67e56960186b3ba0d31e926a5c803b857a80
phiresky <phireskyde+git@gmail.com>
2026-01-27 21:16:42 +0100

    go

--- core/src/main/scala/org/github/srtobi/tradewar3/MusicManager.scala
+++ core/src/main/scala/org/github/srtobi/tradewar3/MusicManager.scala
@@ -1,57 +1,77 @@
 package org.github.srtobi.tradewar3
 
-import org.github.srtobi.tradewar3.audio.{MusicMode, ProceduralMusic}
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.audio.Music
 import scala.compiletime.uninitialized
 
 object MusicManager:
-  private var music: ProceduralMusic = uninitialized
-  private var currentMode: MusicMode = uninitialized
+  private var menuMusic: Music = uninitialized
+  private var gameMusic: Music = uninitialized
+  private var currentMusic: Music = uninitialized
+  private var musicVolume: Float = GameConfig.MUSIC_DEFAULT_VOLUME
 
-  private def ensureInitialized(): Unit =
-    if music == null then
-      music = new ProceduralMusic()
-      music.setVolume(GameConfig.MUSIC_DEFAULT_VOLUME)
+  private def ensureLoaded(): Unit =
+    if menuMusic == null then
+      try
+        menuMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/music/menu.mp3"))
+        menuMusic.setLooping(true)
+        menuMusic.setVolume(musicVolume)
+      catch
+        case e: Exception =>
+          Gdx.app.log("MusicManager", s"Could not load menu music: ${e.getMessage}")
+
+    if gameMusic == null then
+      try
+        gameMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/music/game.mp3"))
+        gameMusic.setLooping(true)
+        gameMusic.setVolume(musicVolume)
+      catch
+        case e: Exception =>
+          Gdx.app.log("MusicManager", s"Could not load game music: ${e.getMessage}")
 
   def playMenuMusic(): Unit =
-    ensureInitialized()
-    if currentMode != MusicMode.Menu then
-      currentMode = MusicMode.Menu
-      music.setMode(MusicMode.Menu)
-      if !isPlaying then music.start()
+    ensureLoaded()
+    if currentMusic == menuMusic && menuMusic != null && menuMusic.isPlaying then return
+    if currentMusic != null && currentMusic.isPlaying then currentMusic.stop()
+    if menuMusic != null then
+      menuMusic.play()
+      currentMusic = menuMusic
 
   def playGameMusic(): Unit =
-    ensureInitialized()
-    if currentMode != MusicMode.Game then
-      currentMode = MusicMode.Game
-      music.setMode(MusicMode.Game)
-      if !isPlaying then music.start()
+    ensureLoaded()
+    if currentMusic == gameMusic && gameMusic != null && gameMusic.isPlaying then return
+    if currentMusic != null && currentMusic.isPlaying then currentMusic.stop()
+    if gameMusic != null then
+      gameMusic.play()
+      currentMusic = gameMusic
 
   def stop(): Unit =
-    if music != null then
-      music.stop()
-      currentMode = null
+    if currentMusic != null then
+      currentMusic.stop()
 
   def pause(): Unit =
-    if music != null then
-      music.stop()
+    if currentMusic != null then
+      currentMusic.pause()
 
   def resume(): Unit =
-    if music != null && currentMode != null then
-      music.start()
-      music.setMode(currentMode)
+    if currentMusic != null then
+      currentMusic.play()
 
   def setVolume(volume: Float): Unit =
-    ensureInitialized()
-    music.setVolume(volume)
+    musicVolume = Math.max(0f, Math.min(1f, volume))
+    if menuMusic != null then menuMusic.setVolume(musicVolume)
+    if gameMusic != null then gameMusic.setVolume(musicVolume)
 
-  def getVolume: Float =
-    if music != null then music.getVolume else GameConfig.MUSIC_DEFAULT_VOLUME
+  def getVolume: Float = musicVolume
 
   def isPlaying: Boolean =
-    music != null && music.isRunning
+    currentMusic != null && currentMusic.isPlaying
 
   def dispose(): Unit =
-    if music != null then
-      music.dispose()
-      music = null
-      currentMode = null
+    if menuMusic != null then
+      menuMusic.dispose()
+      menuMusic = null
+    if gameMusic != null then
+      gameMusic.dispose()
+      gameMusic = null
+    currentMusic = null
